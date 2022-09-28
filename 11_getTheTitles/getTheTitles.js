c3b2533a7f3efc1f243a35f24ebe28a4c6948573
Seok Vin <seok_vin.service@outlook.com>
2022-09-29 08:01:07 +1000

    Complete exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,11 +1,13 @@
 const getTheTitles = function(books) {
+    return books.map(book => { return book.title });
+
+    /* original solution
     let titles = [];
-    
     books.forEach(book => {
         titles.push(book["title"]);
     });
-
     return titles;
+    */
 };
 
 // Do not edit below this line
